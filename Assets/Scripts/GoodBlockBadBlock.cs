d8f431520b7e1b2311ea78fe7884f188ba4b2072
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2023-02-17 15:08:17 -0800

    Started working on goodblockbadblock script

---
+++ Assets/Scripts/GoodBlockBadBlock.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodBlockBadBlock : MonoBehaviour
+{
+    public float timerLength = 1f;
+    public Material otherMat;
+    private Material mat;
+    private float timeLeft;
+    public bool good = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeLeft = timerLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (good)
+        {
+
+        }
+        else
+        {
+
+        }
+
+        if (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        else
+        {
+            good = !good;
+            timeLeft = timerLength;
+        }
+    }
+}
