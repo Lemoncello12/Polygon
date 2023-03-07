00f83123cd509d84241de6dbde936e642f036aed
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2023-03-07 14:22:22 -0800

    Started working on menu save data importing.

---
+++ Assets/Scripts/CheckMenuForUI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckMenuForUI : MonoBehaviour
+{
+    public int sceneNum;
+    public LevelDone? button1;
+    public SecretDone? button2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MenuSaveItems.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSaveItems : MonoBehaviour
+{
+    public bool[] finished = new bool[12];
+    public bool[] secrets = new bool[12];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
