3ec747a3570cc4ad821e51107d0120ec955eba39
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2023-05-30 14:52:23 -0700

    Kept working on Arena

--- Assets/Scripts/EnemyWave.cs
+++ Assets/Scripts/EnemyWave.cs
@@ -8,18 +8,45 @@ public class EnemyWave : MonoBehaviour
     public GameObject wave3;
     public GameObject wave4;
     public GameObject wave5;
-    public GameObject wave6;
-    public GameObject wave7;
-    public GameObject lock2;
+    public GameObject leafo;
+    public int lock2 = 7;
+    public int lock3 = 17;
+    public int lock4 = 29;
+    public int lock5 = 42;
+    public int bossLock = 59;
+    private scoregame scoreGet;
+    private int score;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreGet = GameObject.Find("Player").GetComponent<scoregame>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       score = scoreGet.score;
+        if (score == lock2)
+        {
+            wave2.SetActive(true);
+
+        }
+        if (score == lock3)
+        {
+            wave3.SetActive(true);
+        }
+        if (score == lock4)
+        {
+            wave4.SetActive(true);
+        }
+        if (score == lock5)
+        {
+            wave5.SetActive(true);
+        }
+        if (score == bossLock)
+        {
+            leafo.SetActive(true);
+        }
     }
 }
