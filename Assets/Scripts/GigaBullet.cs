884e6e41a930d638589f20b46d5111949a26c15c
Lemoncello12 <114263635+Lemoncello12@users.noreply.github.com>
2023-06-27 08:31:04 -0700

    Finished boss fight

--- Assets/Scripts/GigaBullet.cs
+++ Assets/Scripts/GigaBullet.cs
@@ -2,35 +2,36 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
+using Random = UnityEngine.Random;
 
 public class GigaBullet : MonoBehaviour
 {
-    public float timerLength = 10f;
+    public float tLength = 10f;
     private AudioSource audio;
-    private float timeRemaining;
+    private float tRemaining;
     public float damage = 50f;
-    Random rnd = Random();
     // Start is called before the first frame update
     void Start()
     {
-        timeRemaining = timerLength;
+        tRemaining = tLength;
         audio = GetComponent<AudioSource>();
-        //transform.Rotate(rnd.Next(-2.5, 2.5) * Time.deltaTime, rnd.Next(-2.5, 2.5) * Time.deltaTime, rnd.Next(-2.5, 2.5) * Time.deltaTime);
+        transform.Rotate(Random.Range(-10f,10f) * Time.deltaTime, Random.Range(-10f, 10f) * Time.deltaTime, Random.Range(-10f, 10f) * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timeRemaining > 0)
+        if (tRemaining > 0)
         {
-            timeRemaining -= Time.deltaTime;
+            tRemaining -= Time.deltaTime;
         }
         else
         {
             gameObject.SetActive(false);
         }
-        transform.Translate(Vector3.forward * 10 * Time.deltaTime);
+        transform.Rotate(Random.Range(-200f, 200f) * Time.deltaTime, Random.Range(-200f, 200f) * 
+            Time.deltaTime, Random.Range(-200f, 200f) * Time.deltaTime);
+        transform.Translate(Vector3.forward * 15 * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
